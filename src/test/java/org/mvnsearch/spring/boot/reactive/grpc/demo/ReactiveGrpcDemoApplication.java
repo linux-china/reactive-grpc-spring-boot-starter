d61b750a81fbbbbf0d54fe5432605b6aa1d56f1e
linux_china <libing.chen@gmail.com>
2019-03-12 20:01:09 -0700

    reactive grpc service demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/reactive/grpc/demo/ReactiveGrpcDemoApplication.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.spring.boot.reactive.grpc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * reactive grpc service demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class ReactiveGrpcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveGrpcDemoApplication.class, args);
+    }
+
+}
