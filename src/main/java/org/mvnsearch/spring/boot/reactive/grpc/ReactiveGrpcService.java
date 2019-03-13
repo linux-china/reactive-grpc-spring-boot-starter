033cae5830fdbc61d7a5105099008da03ed871f2
linux_china <libing.chen@gmail.com>
2019-03-12 20:00:02 -0700

    Reactive gRPC service annotation

---
+++ src/main/java/org/mvnsearch/spring/boot/reactive/grpc/ReactiveGrpcService.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.spring.boot.reactive.grpc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Reactive gRPC service annotation
+ *
+ * @author linux_china
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface ReactiveGrpcService {
+
+}
